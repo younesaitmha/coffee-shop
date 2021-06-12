ea4144b9b5ed97fe9ce7706dfb981a91d34d1ad6
younes ait m'ha <younesaitmha@gmail.com>
2021-06-12 19:55:50 +0100

    adapt the configuration

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -6,9 +6,9 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: '', // the auth0 domain prefix
-    audience: '', // the audience set for the auth0 app
-    clientId: '', // the client id generated for the auth0 app
-    callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
+    url: 'python-dev.us', // the auth0 domain prefix
+    audience: 'coffe shop', // the audience set for the auth0 app
+    clientId: 'lbONo1z85gPfJ1PALqkLzVdNw8Wq8fxJ', // the client id generated for the auth0 app
+    callbackURL: 'http://localhost:8100', // the base url of the running ionic application.
   }
 };
